836f90973a816af70aa7bca92d14bf0a3982e53b
Ujjwal Raut <ujjwalraut.14@gmail.com>
2020-12-06 19:06:08 +0530

    Refactored as per @caelwithcat 's suggestion

--- Hazel/src/Hazel/Core/EntryPoint.h
+++ Hazel/src/Hazel/Core/EntryPoint.h
@@ -6,7 +6,7 @@
 extern Hazel::Application* Hazel::CreateApplication();
 
 extern "C" {
-	__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+	__declspec(dllexport) uint32_t NvOptimusEnablement = 0x00000001;
 }
 
 int main(int argc, char** argv)
